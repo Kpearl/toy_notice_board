b5fd71c736a0ad63bc3d3eab4435cbe5cb904768
Kpearl <kpearl115@gmail.com>
2022-01-24 17:38:05 +0900

    add notice

--- routes/notice.js
+++ routes/notice.js
@@ -12,6 +12,23 @@ class Notice {
     const result = await noticeService.getNotice()
     return result
   }
+
+  async insertNotice() {
+    try {
+      const body = req.body
+      if (body) {
+        const filter = {
+          name: body.name,
+          keyword: body.keyword
+        }
+        const result = await noticeService.insertNotice(filter)
+        return result
+      }
+      return res.json('입력 내용을 확인하세요.')
+    }catch (e) {
+      return res.json('입력 내용을 확인하세요.')
+    }
+  }
 }
 
 new Notice()
--- services/notice.js
+++ services/notice.js
@@ -1,8 +1,14 @@
 const _ = require('lodash')
 
 class noticeService {
-  async getNotice () {
-    return ''
+  async getNotice (id, type) {
+    console.log(id, '에게 ', type, ' 알림 전송')
+    return result
+  }
+
+  async insertNotice (body) {
+    const result = await models.notices.create(body)
+    return result
   }
 }
 
